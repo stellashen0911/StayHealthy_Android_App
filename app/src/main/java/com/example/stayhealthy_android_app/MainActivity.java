053aa5b82427f0fcba3e4d9c615a3c8f86eb0dfc
stellashen0911 <minjieshen0911@gmail.com>
2022-07-15 16:03:02 -0700

    Create the register page

--- app/src/main/java/com/example/stayhealthy_android_app/MainActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.stayhealthy_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Login_btn;
+    private Button register_btn;
+    private EditText login_email;
+    private EditText login_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //set up the buttons
+        Login_btn = findViewById(R.id.button_login);
+        register_btn = findViewById(R.id.button_sign_up);
+        login_email = findViewById(R.id.editTextTextEmailAddress);
+        login_password = findViewById(R.id.editTextTextPassword);
+
+        //open the register page
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    public void openRegisterActivity() {
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/com/example/stayhealthy_android_app/RegisterActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/RegisterActivity.java
@@ -3,12 +3,21 @@ package com.example.stayhealthy_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private Button register_btn;
+    private EditText input_email;
+    private EditText input_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //link the input and button set up
+        register_btn = findViewById(R.id.button_register);
+        input_email = findViewById(R.id.editTextTextEmailAddress_register);
+        input_password = findViewById(R.id.editTextTextPassword_register);
     }
 }

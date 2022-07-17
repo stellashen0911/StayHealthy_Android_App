0b9b9c85cde534a948c611dda24dd7c29965e4af
stellashen0911 <minjieshen0911@gmail.com>
2022-07-16 17:49:33 -0700

    Add verification of username and password input, create the new directory to health record activity

--- app/src/main/java/com/example/stayhealthy_android_app/MainActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.stayhealthy_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button Login_btn;
@@ -23,13 +25,44 @@ public class MainActivity extends AppCompatActivity {
         login_email = findViewById(R.id.editTextTextEmailAddress);
         login_password = findViewById(R.id.editTextTextPassword);
 
-        //open the register page
+
+
+
+        //click the login button
+        Login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+        //click the sign up button and open the register page
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openRegisterActivity();
             }
         });
+    }
+    public void openLoginActivity() {
+        String email_str = login_email.getText().toString();
+        String password_str = login_password.getText().toString();
+        //make sure that the password and username is not empty
+        if (email_str.isEmpty() || password_str.isEmpty()) {
+            Context context = getApplicationContext();
+            CharSequence text = "Username and password cannot be empty!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        //validate the password and username in the firebase authentication
+
+
+        //if the authentication is correct open the health record page
+        Intent healthRecordIntent = new Intent(this, HealthRecordActivity.class);
+        startActivity(healthRecordIntent);
     }
 
     public void openRegisterActivity() {
--- app/src/main/java/com/example/stayhealthy_android_app/RegisterActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/RegisterActivity.java
@@ -2,9 +2,13 @@ package com.example.stayhealthy_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button register_btn;
@@ -19,5 +23,36 @@ public class RegisterActivity extends AppCompatActivity {
         register_btn = findViewById(R.id.button_register);
         input_email = findViewById(R.id.editTextTextEmailAddress_register);
         input_password = findViewById(R.id.editTextTextPassword_register);
+
+        //set the listener for the register button
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register_user();
+            }
+        });
+
     }
+
+    public void register_user() {
+        String email_str = input_email.getText().toString();
+        String password_str = input_password.getText().toString();
+        //make sure that the password and username is not empty
+        if (email_str.isEmpty() || password_str.isEmpty()) {
+            Context context = getApplicationContext();
+            CharSequence text = "Username and password cannot be empty!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        //add this username and password on firebase
+
+
+
+        //if the authentication is correct open the health record page
+        Intent healthRecordIntent = new Intent(this, HealthRecordActivity.class);
+        startActivity(healthRecordIntent);
+    }
+
 }

ca880e70c2c2e2fba7cf80171f1a7682dcdad356
wss8013 <wss8013@gmail.com>
2022-07-20 22:50:11 -0700

    add water activity

---
+++ app/src/main/java/com/example/stayhealthy_android_app/WaterActivity.java
@@ -0,0 +1,13 @@
+package com.example.stayhealthy_android_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WaterActivity  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

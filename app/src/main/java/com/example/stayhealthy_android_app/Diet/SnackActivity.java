4fb11977c4cc905d5d78f092e3c10b7e6645ed51
Charlio Xu <charlioxu@pop-os.localdomain>
2022-07-25 23:31:30 -0700

    added diet

---
+++ app/src/main/java/com/example/stayhealthy_android_app/Diet/SnackActivity.java
@@ -0,0 +1,16 @@
+package com.example.stayhealthy_android_app.Diet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.stayhealthy_android_app.R;
+
+public class SnackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snack);
+    }
+}

6df1e3a81421399767e7fbcd18b325fe8e1d6a18
stellashen0911 <minjieshen0911@gmail.com>
2022-08-01 18:57:52 -0700

    edit profile menu drawer click listeners, solve existing bugs

--- app/src/main/java/com/example/stayhealthy_android_app/AwardActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/AwardActivity.java
@@ -1,6 +1,7 @@
 package com.example.stayhealthy_android_app;
 
 import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,6 +9,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,12 +18,13 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener;
 
 public class AwardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView bottomNavigationView;
     private DrawerLayout drawer;
     private Toolbar toolbar;
-    private NavigationView nav_view;
+    private NavigationView profile_nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +35,18 @@ public class AwardActivity extends AppCompatActivity implements NavigationView.O
         initWidgets();
         setBottomNavigationView();
 
-        // Initialize toolbar
-        initProfile();
-        nav_view = findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(this);
-    }
-
-    private void initProfile() {
-        // Initialize toolbar
+        // Initialize profile drawer
+        drawer = findViewById(R.id.drawer_layout);
+        profile_nv = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        profile_nv.setNavigationItemSelectedListener(this);
+        System.out.println("profile init finished");
     }
 
     private void initWidgets() {
@@ -68,19 +68,6 @@ public class AwardActivity extends AppCompatActivity implements NavigationView.O
             super.onBackPressed();
         }
     }
-
-//    private void setupDrawerContent(NavigationView navigationView) {
-//        System.out.println("here start setup");
-//        navigationView.setNavigationItemSelectedListener(
-//                new NavigationView.OnNavigationItemSelectedListener() {
-//                    @Override
-//                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-//                        System.out.println("here 1");
-//                        selectDrawerItem(menuItem);
-//                        return true;
-//                    }
-//                });
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -117,40 +104,42 @@ public class AwardActivity extends AppCompatActivity implements NavigationView.O
         });
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
         // Create a new fragment and specify the fragment to show based on nav item clicked
+        System.out.println("clicked");
         Fragment fragment = null;
         Class fragmentClass = null;
-        System.out.println("here 2");
+        System.out.println(item.getItemId());
         switch(item.getItemId()) {
             case R.id.nav_settings:
-                System.out.println("here 4");
                 fragmentClass = SettingFragment.class;
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                    System.out.println("here 4");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
             case R.id.nav_health_records:
                 System.out.println("here 3");
                 drawer.closeDrawers();
                 startActivity(new Intent(getApplicationContext(), HealthRecordActivity.class));
                 break;
             case R.id.nav_award:
+                System.out.println("clicked award");
                 drawer.closeDrawers();
                 startActivity(new Intent(getApplicationContext(), AwardActivity.class));
                 break;
             case R.id.nav_journey:
+                System.out.println("clicked journey");
                 drawer.closeDrawers();
                 startActivity(new Intent(getApplicationContext(), JourneyActivity.class));
                 break;
-            default:
-                fragmentClass = null;
-                drawer.closeDrawers();
         }
 
         if (fragment != null) {
-            try {
-                fragment = (Fragment) fragmentClass.newInstance();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
             System.out.println("here 5");
             // Insert the fragment by replacing any existing fragment
             FragmentManager fragmentManager = getSupportFragmentManager();
@@ -163,6 +152,8 @@ public class AwardActivity extends AppCompatActivity implements NavigationView.O
             // Close the navigation drawer
             drawer.closeDrawer(GravityCompat.START);
         }
+        System.out.println("here 6");
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }

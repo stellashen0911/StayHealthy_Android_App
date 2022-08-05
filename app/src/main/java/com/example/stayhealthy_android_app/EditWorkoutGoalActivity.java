d6dc19a4e87e87d030411e1334964d768d674d9d
stellashen0911 <minjieshen0911@gmail.com>
2022-08-05 15:31:55 -0700

    Add edit workout goal spinner listener and layout setup

--- app/src/main/java/com/example/stayhealthy_android_app/EditWorkoutGoalActivity.java
+++ app/src/main/java/com/example/stayhealthy_android_app/EditWorkoutGoalActivity.java
@@ -2,9 +2,12 @@ package com.example.stayhealthy_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -26,6 +29,11 @@ public class EditWorkoutGoalActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DatabaseReference myDataBase;
     private Spinner workout_numbers_selection;
+    private int totalNumberOfWorkout;
+    private CardView CV1;
+    private CardView CV2;
+    private CardView CV3;
+    private CardView CV4;
 
 
     @Override
@@ -49,11 +57,77 @@ public class EditWorkoutGoalActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         myDataBase = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
 
+        //set up the card view of detailed workout
+        CV1 = (CardView) findViewById(R.id.workoutGoalCV_01);
+        CV2 = (CardView) findViewById(R.id.workoutGoalCV_02);
+        CV3 = (CardView) findViewById(R.id.workoutGoalCV_03);
+        CV4 = (CardView) findViewById(R.id.workoutGoalCV_04);
+        CV1.setVisibility(View.GONE);
+        CV2.setVisibility(View.GONE);
+        CV3.setVisibility(View.GONE);
+        CV4.setVisibility(View.GONE);
+
         //set up the workout number selection
         workout_numbers_selection = findViewById(R.id.spinner_select_activity_numbers);
         ArrayAdapter<CharSequence> workout_numbers_selection_adapter = ArrayAdapter.createFromResource(this, R.array.workout_activity_numbers, android.R.layout.simple_spinner_item);
         workout_numbers_selection_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         workout_numbers_selection.setAdapter(workout_numbers_selection_adapter);
+
+        workout_numbers_selection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = workout_numbers_selection.getSelectedItem().toString();
+                System.out.println("text is "+ text);
+                if (text.equals("1 workout activity")) {
+                    totalNumberOfWorkout = 1;
+                } else if (text.equals("2 workout activities")) {
+                    totalNumberOfWorkout = 2;
+                } else if (text.equals("3 workout activities")) {
+                    totalNumberOfWorkout = 3;
+                } else if (text.equals("4 workout activities")) {
+                    totalNumberOfWorkout = 4;
+                } else {
+                    totalNumberOfWorkout = 0;
+                }
+                updateCardViewNumber();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                totalNumberOfWorkout = 0;
+            }
+        });
+
+
+    }
+
+    private void updateCardViewNumber() {
+            if (totalNumberOfWorkout == 1) {
+                CV1.setVisibility(View.VISIBLE);
+                CV2.setVisibility(View.GONE);
+                CV3.setVisibility(View.GONE);
+                CV4.setVisibility(View.GONE);
+            } else if (totalNumberOfWorkout == 2) {
+                CV1.setVisibility(View.VISIBLE);
+                CV2.setVisibility(View.VISIBLE);
+                CV3.setVisibility(View.GONE);
+                CV4.setVisibility(View.GONE);
+            } else if (totalNumberOfWorkout == 3) {
+                CV1.setVisibility(View.VISIBLE);
+                CV2.setVisibility(View.VISIBLE);
+                CV3.setVisibility(View.VISIBLE);
+                CV4.setVisibility(View.GONE);
+            } else if (totalNumberOfWorkout == 4) {
+                CV1.setVisibility(View.VISIBLE);
+                CV2.setVisibility(View.VISIBLE);
+                CV3.setVisibility(View.VISIBLE);
+                CV4.setVisibility(View.VISIBLE);
+            } else {
+                CV1.setVisibility(View.GONE);
+                CV2.setVisibility(View.GONE);
+                CV3.setVisibility(View.GONE);
+                CV4.setVisibility(View.GONE);
+            }
     }
 
     private void initDate() {
